18f0982aff57dc7861bf98e64eaf525aefc3850a
BethelehemE <41347779+BethelehemE@users.noreply.github.com>
2018-08-06 13:46:02 -0700

    Update about.js

--- about.js
+++ about.js
@@ -1,16 +1,16 @@
 
 function changePic1(){
-  document.getElementById("shirt1").src = "IMG_0705.JPG";
+  document.getElementById("shirt1").src = "pictures/IMG_0705.JPG";
 }
 
 function changePic2(){
-  document.getElementById("shirt1").src = "IMG_0704.JPG";
+  document.getElementById("shirt1").src = "pictures/IMG_0704.JPG";
 }
 
 function changePic3(){
-  document.getElementById("thorn").src = "thorns.jpg";
+  document.getElementById("thorn").src = "pictures/thorns.jpg";
 }
 
 function changePic4(){
-  document.getElementById("thorn").src = "IMG_0704.JPG";
+  document.getElementById("thorn").src = "pictures/IMG_0704.JPG";
 }
