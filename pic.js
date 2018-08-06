ed46b0a120972d22bcce5e145503c8b441a3fda5
BethelehemE <41347779+BethelehemE@users.noreply.github.com>
2018-08-06 13:16:14 -0700

    Add files via upload

---
+++ pic.js
@@ -0,0 +1,16 @@
+
+function changePic1(){
+  document.getElementById("shirt1").src = "IMG_0705.JPG";
+}
+
+function changePic2(){
+  document.getElementById("shirt1").src = "IMG_0704.JPG";
+}
+
+function changePic3(){
+  document.getElementById("thorn").src = "thorns.jpg";
+}
+
+function changePic4(){
+  document.getElementById("thorn").src = "IMG_0704.JPG";
+}
